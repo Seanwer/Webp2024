e933390531c34c2512e9667300c961e2d5e8bc6a
Seanwer <160708442+Seanwer@users.noreply.github.com>
2024-05-01 15:49:05 +0800

    Add files via upload

---
+++ lab4/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CGU_Login from './cgu_login'
+
+function App() {
+  return (
+    <div className="App">
+      {CGU_Login() }
+    </div>
+  );
+}
+
+export default App;
