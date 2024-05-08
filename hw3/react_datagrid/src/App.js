0c20f7a165a336156d55ed7a0d7c205695e65399
Seanwer <160708442+Seanwer@users.noreply.github.com>
2024-05-08 16:26:13 +0800

    Add files via upload

---
+++ hw3/react_datagrid/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import cgu from './cgu';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        { cgu() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
